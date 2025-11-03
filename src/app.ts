ffbc008f861411a7c1b165708c6b9ba530c71b55
Virgile Marty <marty.virgile@gmail.com>
2025-11-03 12:13:53 +0100

    add notFound + linter / prettier

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from 'express';
+import { notFound } from './middleware/notFound';
 
 const app = express();
 const router = express.Router();
 
-router.get("/", (_req: Request, res: Response) => res.send("Hello World"));
+router.get('/', (_req: Request, res: Response) => res.send('Hello World'));
 
 app.use(router);
+app.use(notFound);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import { createServer, Server } from "http";
-import app from "./app";
-import "dotenv/config";
-
+import 'dotenv/config';
+import { createServer, Server } from 'http';
+import app from './app';
 
 const server: Server = createServer(app);
 const port: number = Number(process.env.PORT) ?? 3000;
 
 server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
+  console.log(`Server is running on http://localhost:${port}`);
 });
