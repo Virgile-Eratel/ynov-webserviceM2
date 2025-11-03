26e8a2525bcdb2c9dcc6f297f6e88cec899ae803
Virgile Marty <marty.virgile@gmail.com>
2025-11-03 12:39:56 +0100

    route health

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express';
-import { notFound } from './middleware/notFound';
+import { notFound } from './middlewares';
+import health from './routes/health';
 
 const app = express();
 const router = express.Router();
 
 router.get('/', (_req: Request, res: Response) => res.send('Hello World'));
 
+app.use(health);
 app.use(router);
 app.use(notFound);
 
