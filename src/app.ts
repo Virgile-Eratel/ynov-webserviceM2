0b443490f6d73719cfd1652a2818ce348269e7fd
Virgile Marty <marty.virgile@gmail.com>
2025-11-24 16:07:08 +0100

    pakages securité

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,29 @@ import pinoHttp from './middlewares/pinoHttp.middleware';
 import routes from './routes';
 import health from './routes/health';
 import limiter from './middlewares/limiter.middleware';
+import helmet from 'helmet';
+import cors from 'cors';
+import { CorsOptions } from 'cors';
+
+const corsOptions: CorsOptions = {
+  origin: ['http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
 const app = express();
 
 const swaggerJsdoc = require('swagger-jsdoc');
 const specs = swaggerJsdoc(swaggerOptions);
 
+//sécurité
 app.use(limiter);
+app.use(helmet());
+app.use(cors(corsOptions));
+
+//log
 app.use(pinoHttp);
+
 app.use(express.json({}));
 app.use(health);
 
